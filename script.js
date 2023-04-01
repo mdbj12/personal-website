2991ca311ab4bc705b3cda520178837251e4026b
Michael Jeung <michaelj229@gmail.com>
2023-04-01 14:51:27 -0400

    buttons created/created links for each button

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content is added by JavaScript";
-// document.querySelector("body").appendChild(h2);
+const newDiv = document.createElement('div')
+const newP = document.createElement('p')
+const newH1 = document.createElement('h1')
+const newH2 = document.createElement('h2')
+
+const btn1 = document.querySelector('.button1')
+btn1.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.href = "../HTML/home.html"
+})
+const btn2 = document.querySelector('.button2')
+btn2.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.href = "../HTML/about.html"
+})
